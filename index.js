ea922b1f8a36ebaf023dd46d068dc713d7f60fb2
Whispering Code <74275864+luuizpeedro@users.noreply.github.com>
2025-05-26 11:04:40 -0300

    Update Files Code Editor

--- index.js
+++ index.js
@@ -1,59 +1,114 @@
-// Exibir hora e data em tempo real
-function atualizarHora() {
-  const currentTime = new Date();
-  const hora = currentTime.toLocaleTimeString();
-  document.getElementById("current-time").textContent = hora;
-}
-setInterval(atualizarHora, 1000);
+// Catching commonly used elements to minimize dom queries
+const livePreviewFrame = document.getElementById("live-preview");
+const htmlEditor = document.getElementById("html");
+const cssEditor = document.getElementById("css");
+const jsEditor = document.getElementById("js");
 
-// Mensagens de boas-vindas
-window.onload = () => {
-  const chatBox = document.getElementById("chat-box");
-  const boasVindas = `
-  Bem-vindo ao sistema!
-  -----------------------
-  Sistema operacional: Terminal Hack
-  Usuário: root
-  Computador: hacker-pc
-  Pressione ENTER para começar.
-  `;
-  chatBox.textContent = boasVindas;
-  chatBox.scrollTop = chatBox.scrollHeight; // Rola até o final da tela
-};
+// Function to set up the live preview iframe and include necessary scripts
+function initializeLivePreview() {
+  livePreviewFrame.contentWindow.document.body.innerHTML = "";
+  const styleElement = document.createElement("style");
+  styleElement.setAttribute("id", "live-preview-style");
+  livePreviewFrame.contentWindow.document.head.appendChild(styleElement);
 
-// Função para exibir as mensagens no chat (com efeito de terminal)
-function exibirMensagem(mensagem, tipo) {
-  const chatBox = document.getElementById("chat-box");
-  const div = document.createElement("div");
-  div.classList.add("message", tipo);
-  div.textContent = mensagem;
-  chatBox.appendChild(div);
-  chatBox.scrollTop = chatBox.scrollHeight; // Rolagem automática para o final
+  const pagedJsScript = document.createElement("script");
+  pagedJsScript.src = "https://unpkg.com/pagedjs/dist/paged.legacy.polyfill.js";
+  livePreviewFrame.contentWindow.document.head.appendChild(pagedJsScript);
 }
 
-// Função para enviar a mensagem do usuário
-async function enviarMensagem() {
-  const userInput = document.getElementById("user-input");
-  const mensagem = userInput.value.trim();
-
-  if (mensagem) {
-    // Exibe a mensagem do usuário
-    exibirMensagem(`user@hacker:~$ ${mensagem}`, "user");
-    userInput.value = ""; // Limpa o campo de entrada
-
-    // Simula a resposta do bot
-    exibirMensagem("... Digite algo mais...", "bot");
-    setTimeout(() => {
-      exibirMensagem('Resposta simulada: "Comando reconhecido!"', "bot");
-    }, 1500); // Tempo de "digitação" para o efeito
-  }
+// Function to update the live preview iframe with the html code from editor
+function updateLiveHTMLPreview(codeEditors) {
+  livePreviewFrame.contentWindow.document.body.innerHTML =
+    codeEditors.html.getValue();
 }
 
-// Captura o "Enter" ou o clique do botão para enviar a mensagem
-document
-  .getElementById("user-input")
-  .addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      enviarMensagem();
+// Function to update the live preview iframe with the css code from editor
+function updateLiveCSSPreview(codeEditors) {
+  const styleElement =
+    livePreviewFrame.contentWindow.document.getElementById(
+      "live-preview-style"
+    );
+  styleElement.innerHTML = codeEditors.css.getValue();
+}
+
+// Function to update the live preview iframe with the js code from editor
+function updateLiveJSPreview(codeEditors) {
+  const scriptElement = document.createElement("script");
+  scriptElement.innerHTML = codeEditors.js.getValue();
+  livePreviewFrame.contentWindow.document.body.appendChild(scriptElement);
+}
+
+// Function to initialize CodeMirror editors for html, css and javascript
+function initializeCodeEditors() {
+  function getDefaultOptions(object) {
+    const defaultOptions = {
+      lineNumbers: true,
+      autoCloseTags: true,
+      autoCloseBrackets: true,
+      theme: "panda-syntax",
+    };
+    if (object) {
+      const keys = Object.keys(object);
+      for (const key of keys) {
+        defaultOptions[key] = object[key];
+      }
     }
+    return defaultOptions;
+  }
+
+  const codeEditors = {
+    html: CodeMirror(
+      htmlEditor,
+      getDefaultOptions({
+        mode: "text/html",
+        value: "",
+      })
+    ),
+    css: CodeMirror(
+      cssEditor,
+      getDefaultOptions({
+        mode: "css",
+        value: "",
+        extraKeys: { "Ctrl-Space": "autocomplete" },
+        hintOptions: {
+          completeSingle: false,
+          closeOnUnfocus: false,
+        },
+      })
+    ),
+    js: CodeMirror(
+      jsEditor,
+      getDefaultOptions({
+        mode: "javascript",
+        value: "",
+      })
+    ),
+  };
+  return codeEditors;
+}
+
+// Function to set up the live preview studio with CodeMirror editors and event listeners
+function setupLivePreviewStudio() {
+  const codeEditors = initializeCodeEditors();
+
+  // Event listener for changes in HTML editor
+  CodeMirror.on(codeEditors.html, "change", () => {
+    updateLiveHTMLPreview(codeEditors);
   });
+
+  // Event listener for changes in CSS editor
+  CodeMirror.on(codeEditors.css, "change", () => {
+    updateLiveCSSPreview(codeEditors);
+  });
+
+  // Event listener for changes in HTML editor
+  CodeMirror.on(codeEditors.js, "change", () => {
+    updateLiveJSPreview(codeEditors);
+  });
+}
+
+// Event listener to set up the live preview studio after the dom is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  initializeLivePreview();
+  setupLivePreviewStudio();
+});
